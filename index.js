f4a060da5e96f22f302ce0fb7c7788dad030e6d9
AnthonyPillow <anthonypillow18@gmail.com>
2020-03-26 11:16:37 -0700

    Added package.json with dependencies and updated index.js file with more prompts leading to functions that will query the db.

--- index.js
+++ index.js
@@ -6,5 +6,92 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "root",
-    database: "employeeDB"
+    database: "companyDB"
 })
+
+function startPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Run employee tracker?",
+                choices: ["Run", "Cancel"],
+                name: "confirm"
+            }
+        ]).then((data) => {
+            if (data.confirm === "Run") {
+                console.log("yee")
+                viewOptions()
+            } else {
+                console.log("Nee")
+                return
+            }
+        })
+}
+
+startPrompt()
+
+function viewOptions() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Choose Option",
+                choices: ["View company departments, roles, employees", "Create departments, roles, employees", "Update Employee Roles"],
+                name: "choice"
+            }
+        ]).then((data) => {
+            switch (data.choice) {
+                case "View company departments, roles, employees": view()
+                    break
+
+                case "Create departments, roles, employees": create()
+                    break
+
+                case "Update Employee Roles": update()
+                    break
+            }
+        })
+}
+
+function view() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Choose Option",
+                choices: ["View company departments", "View company employee roles", "View Company Employees"],
+                name: "choice"
+            }
+        ]).then((data) => {
+            switch (data.choice) {
+                case "View company departments": viewDepartments()
+                break
+                case "View company employee roles": viewRoles()
+                break
+                case "View Company Employees": viewEmployees()
+                break
+            }
+        })
+}
+
+function create() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Choose Option",
+                choices: ["Create company departments", "Create company employee roles", "Create Company Employees"],
+                name: "choice"
+            }
+        ]).then((data) => {
+            switch (data.choice) {
+                case "Create company departments": createDepartments()
+                break
+                case "Create company employee roles": createRoles()
+                break
+                case "Create Company Employees": createEmployees()
+                break
+            }
+        })
+}
